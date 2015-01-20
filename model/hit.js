6b968408de1284ff06393d95f0a34102addfcf0e
Geoffrey Plitt <geoff@gweb.org>
2015-01-20 15:47:55 -0800

    better error message

--- model/hit.js
+++ model/hit.js
@@ -133,7 +133,7 @@ module.exports = function(config) {
       if (! HIT.prototype.nodeExists(['DisposeHITResult', 'Request', 'IsValid'], response)) { callback([new Error('No "DisposeHITResult > Request > IsValid" node on the response')]); return; }
       if (response.DisposeHITResult.Request.IsValid.toLowerCase() != 'true') {
         //console.error('ERROR(alantrrs/mturk--model/hit:135): Reponse was: ', JSON.stringify(response));
-        callback([new Error('Response says DisposeHITResult is invalid')]);
+        callback([new Error('Response says DisposeHITResult is invalid: ' + response.DisposeHITResult.Request.Errors.Error.Message)]);
         return;
       }
 
